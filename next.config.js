e3baec015d2376100040d9b763fbe2e66c0ccb1b
Steven PC <stevenoh2003@gmail.com>
2024-03-10 14:09:19 +0900

    disable eslint across entire project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  
+  eslint: {
+    // Warning: This will disable ESLint checks for the entire project.
+    ignoreDuringBuilds: true,
+  },
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: [
+      "www.notion.so",
+      "lh5.googleusercontent.com",
+      "s3-us-west-2.amazonaws.com",
+    ],
   },
 }
