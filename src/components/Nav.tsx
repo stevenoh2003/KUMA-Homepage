25d37b343fadb048619acca1451377797945dffc
Steven <stevenoh2003@gmail.com>
2024-05-28 23:51:09 +0900

    Fixed inccorect link to events page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -24,7 +24,7 @@ export default function NavBar() {
     },
     {
       title: t("nav.event"),
-      path: "/project",
+      path: "/events",
       current: router.pathname === "/events",
     },
   ]
